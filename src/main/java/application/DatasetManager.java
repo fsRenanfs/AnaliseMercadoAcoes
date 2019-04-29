0fea647341ac2c7b9104b453dcffad7035dcffdc
renan.f.santos <rferreirasantos98@gmail.com>
2019-04-29 13:53:20 -0300

    Refactor union datasets

--- src/main/java/application/DatasetManager.java
+++ src/main/java/application/DatasetManager.java
@@ -1,11 +1,18 @@
 package application;
 
 import application.conversor.ProcessarArquivos;
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder$;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
+
+import static com.sun.tools.doclint.Entity.lambda;
 
 public class DatasetManager {
     private SparkSession sparkSession;
@@ -14,30 +21,48 @@ public class DatasetManager {
         this.sparkSession = sparkSession;
     }
 
-    public ArrayList<Dataset<Row>> getDatasetArquivoProcessado(ProcessarArquivos processarArquivos) {
+    public Dataset<Row> getDatasetArquivoProcessado(ProcessarArquivos processarArquivos) {
         ArrayList<Dataset<Row>> datasets = new ArrayList<>();
         //Cria os DataSets dos arquivos
         for (String csvPath : processarArquivos.getCSVsPaths()) {
             Dataset<Row> csv = sparkSession.read().format("csv").option("header", "true").load(csvPath);
             datasets.add(csv);
         }
-        return datasets;
+        return unirDatasets(datasets);
     }
 
-    //Retorna um dataset contendo a informacao da valorizacao dos ativos de todos os datasets
-    public Dataset<Row> getDatasetValorizacaoAtivos(ArrayList<Dataset<Row>> datasets) {
-        for (int i = 0; i < datasets.size(); i++) {
-            //Registra a variacao por dia
-            datasets.set(i, getDatasetInfo(datasets.get(i), "Dados",
-                    "select *, (preco_fechamento-preco_abertura) as variacao from Dados"));
+    //Retorna um dataset contendo a informacao da valorizacao dos ativos de todos os datasets por mes
+    public Dataset<Row> getDatasetValorizacao(Dataset<Row> dataset) {
+        //  Dataset<Row> datasetPrecoAbertura = ;
 
-            //Registra a variacao por mes
-            datasets.set(i, getDatasetInfo(datasets.get(i), "DadosMensais",
-                    "select distinct ano as ano, mes as mes, codigo_negociacao as codigo_negociacao, descricao_negociacao as descricao_negociacao, prazo_dias_mercado as prazo_dias_mercado, sum(variacao) as variacao from DadosMensais" +
-                            "        Group by ano, mes, codigo_negociacao, descricao_negociacao, prazo_dias_mercado"));
-        }
+        //Registra a variacao por dia
+        dataset = getDatasetVariacaoDia(dataset);
 
-        return unirDatasets(datasets);
+        //Registra a variacao por mes
+        dataset = getDatasetVariacaoMes(dataset);
+
+        return dataset;
+    }
+
+    //Retorna um dataset com os precos de abertura de cada mes
+    private Dataset<Row> getPrecoAberturaMeses(Dataset<Row> datasetVariacaoDia) {
+        Dataset<Row> dataset = getDatasetInfo(datasetVariacaoDia, "DataInicioMes",
+                "select  ano, mes, preco_abertura from DataInicioMes " +
+                        "        Order by ano, mes Asc");
+     //   dataset.map
+        //  dataset.map();
+        return dataset;
+    }
+
+    private Dataset<Row> getDatasetVariacaoMes(Dataset<Row> datasetVariacaoDia) {
+        return getDatasetInfo(datasetVariacaoDia, "DadosMensais",
+                "select distinct ano as ano, mes as mes, codigo_negociacao as codigo_negociacao, descricao_negociacao as descricao_negociacao, prazo_dias_mercado as prazo_dias_mercado, sum(variacao) as variacao from DadosMensais" +
+                        "        Group by ano, mes, codigo_negociacao, descricao_negociacao, prazo_dias_mercado");
+    }
+
+    private Dataset<Row> getDatasetVariacaoDia(Dataset<Row> dataset) {
+        return getDatasetInfo(dataset, "Dados", "select *, (preco_fechamento-preco_abertura) as variacao from Dados");
+
     }
 
     private Dataset<Row> getDatasetInfo(Dataset<Row> dataset, String tempTable, String querySQL) {
--- src/main/java/application/main.java
+++ src/main/java/application/main.java
@@ -5,7 +5,6 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-import javax.xml.crypto.Data;
 import java.util.ArrayList;
 
 public class main {
@@ -13,19 +12,18 @@ public class main {
     public static void main(String[] args) {
         SparkSession sparkSession = SparkSession.builder().appName("application.main").config("spark.master", "local").getOrCreate();
         DatasetManager datasetManager = new DatasetManager(sparkSession);
-        ArrayList<Dataset<Row>> datasets;
-        Dataset<Row> datasetValorizacaoAtivos;
+        Dataset<Row> dataset, datasetValorizacao;
 
         //Converte os arquivos do diretorio para CSV
         ProcessarArquivos processarArquivos = new ProcessarArquivos(args[0]);
 
         //Retorna os DataSets dos arquivos processados
-        datasets = datasetManager.getDatasetArquivoProcessado(processarArquivos);
+        dataset = datasetManager.getDatasetArquivoProcessado(processarArquivos);
 
         //Retorna um Dataset com as informacoes da valorizacao dos titulos nos anos
-        datasetValorizacaoAtivos = datasetManager.getDatasetValorizacaoAtivos(datasets);
+        datasetValorizacao = datasetManager.getDatasetValorizacao(dataset);
 
-        datasetValorizacaoAtivos.filter("codigo_negociacao = 'ITUB4'").orderBy("ano","mes").show(100);
+        datasetValorizacao.filter("codigo_negociacao = 'SANB11'").orderBy("ano", "mes").show(100);
 
         sparkSession.stop();
         sparkSession.close();
